7c5efc6c53cd5c322b91a2f825a767ce5925ce1f
Vinicius Bazanella <vbazanella@NELOGICA228.local>
2022-09-09 19:41:03 -0300

    Implementa solucao do trabalho

--- features/combiner.feature
+++ features/combiner.feature
@@ -4,3 +4,8 @@ Feature: Combiner
         Given A string "Tpo" and another string "oCder"
         When I call combiner passing both strings as parameters
         Then The result should be "TopCoder"
+
+    Scenario: Combine aa and bb
+        Given A string "aa" and another string "bb"
+        When I call combiner passing both strings as parameters
+        Then The result should be "abab"
--- features/support/steps.js
+++ features/support/steps.js
@@ -2,12 +2,13 @@ const assert = require("assert");
 const { When, Then, Given } = require("@cucumber/cucumber");
 const { combiner } = require("../../src/solutions");
 
-Given("A string {string} and another string {string}", function () {
-  return "TopCoder";
+Given("A string {string} and another string {string}", function (str1, str2) {
+  this.str1 = str1;
+  this.str2 = str2;
 });
 
 When("I call combiner passing both strings as parameters", function () {
-  this.result = combiner("Tpo", "oCder");
+  this.result = combiner(this.str1, this.str2);
 });
 
 Then("The result should be {string}", function (expectedResponse) {
